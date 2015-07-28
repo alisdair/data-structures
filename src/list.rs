a78d3150668614d65ca13d49becb4b9f1c81ef29
Alisdair McDiarmid <alisdair@mcdiarmid.org>
2015-07-28 23:57:26 +0100

    Add linked list, courtesy of "Too Many Lists"
    http://cglab.ca/~abeinges/blah/too-many-lists/book/second.html

---
+++ src/list.rs
@@ -0,0 +1,61 @@
+pub struct List<T> {
+    head: Link<T>,
+}
+
+type Link<T> = Option<Box<Node<T>>>;
+
+struct Node<T> {
+    item: T,
+    next: Link<T>,
+}
+
+impl<T> List<T> {
+    pub fn new() -> Self {
+        List { head: None }
+    }
+
+    pub fn push(&mut self, item: T) {
+        let node = Box::new(Node {
+            item: item,
+            next: self.head.take(),
+        });
+
+        self.head = Some(node);
+    }
+
+    pub fn pop(&mut self) -> Option<T> {
+        self.head.take().map(|node| {
+            let node = *node;
+            self.head = node.next;
+            node.item
+        })
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::List;
+
+    #[test]
+    fn basics() {
+        let mut list = List::new();
+
+        assert_eq!(list.pop(), None);
+
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        assert_eq!(list.pop(), Some(3));
+        assert_eq!(list.pop(), Some(2));
+
+        list.push(4);
+        list.push(5);
+
+        assert_eq!(list.pop(), Some(5));
+        assert_eq!(list.pop(), Some(4));
+
+        assert_eq!(list.pop(), Some(1));
+        assert_eq!(list.pop(), None);
+    }
+}
